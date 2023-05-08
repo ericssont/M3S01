49834c3f0097ffb7389bf923f1a1d10d7e839e4a
Ericsson Tribess <ericktribess@gmail.com>
2023-05-08 16:05:26 -0300

    first commit

---
+++ src/main/java/br/com/m3s01exercicio_semana/m3s01/M3S01Application.java
@@ -0,0 +1,13 @@
+package br.com.m3s01exercicio_semana.m3s01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3S01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3S01Application.class, args);
+	}
+
+}
